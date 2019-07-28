7e00520ea3592c07ddde0ed8e25b071ffa1f2c0c
hong <809817580@qq.com>
2019-07-28 18:45:16 +0800

    新增快速排序

--- python/sort.py
+++ python/sort.py
@@ -1,3 +1,8 @@
+import math
+import time
+import random
+
+
 def bubbleSort(arr):
     """
     BubbleSort 冒泡排序
@@ -79,8 +84,58 @@ def shellSotr(arr):
         n = n // 2
 
 
+def quickSortRecursion(arr, left, right):
+    """
+    The recursion of quickSort
+    :param arr:
+    :param left: 此次排序数组范围的最左索引
+    :param right: 最右索引
+    :return:
+    """
+    mid = arr[(right + left) // 2]  # 取数组中间位置的数值作为划分左右区域的依据
+    LEFT = left  # 将输入的left固定住，用于递归的输入
+    RIGHT = right  # 同上
+
+    while left < right:
+        while arr[left] < mid:
+            left += 1
+        while arr[right] > mid:
+            right -= 1
+        arr[left], arr[right] = arr[right], arr[left]
+    # 确定最终中间数值mid的位置
+    if arr[left] == mid:
+        MID = left
+    else:
+        MID = right
+
+    # 对划分后的左边区域和右边区域分别进行快速排序的递归
+    # 当左边或右边区域只剩下一个元素时，则无需再进行排序了
+    if (RIGHT - MID) > 1:
+        quickSortRecursion(arr, MID+1, RIGHT)
+    if (MID - LEFT) > 1:
+        quickSortRecursion(arr, LEFT, MID-1)
+
+
+def quickSort(arr):
+    """
+    quickSort 快速排序
+    :param arr: 待排序数组
+    :return:
+    """
+    quickSortRecursion(arr, 0, len(arr)-1)
+
+
 if __name__ == '__main__':
+    # 检验算法的正确性
     l = [8, 9, 1, 7, 2, 3, 5, 4, 6, 0]
     print(l)
-    shellSotr(l)
+    quickSort(l)
     print(l)
+
+    # 检验算法的运行时间
+    n = 100000
+    l = [random.randint(0, n) for i in range(n)]
+    s1 = time.time()
+    quickSort()
+    s2 = time.time()
+    print(s2 - s1)
